fbd67b931e0528bd89cb398c8a91b094c4498276
Denis Kazberuk <Deniskazberuk@hotmail.com>
2022-10-31 00:23:35 +0100

    RageQuit atm

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -2,9 +2,11 @@ const auth = require('../middleWare/authentication.js');
 
 const lists = async function (fastify) {
 	fastify.register(auth);
+	// const l = Object.entries(list.dict)
+	const Lul = await fastify.mongo.db.collection('lists');
 	await fastify.post('/lists', async (request, reply) => {
-		console.log("I'm in the lists yes!");
-		console.log(request.user);
+		const kekW = await Lul.find({}).toArray() // LA PUTAIN DE TA MÈRE COMMENT CA IL FAUT TO ARRAY FDPPPPPPPPPPPPPPPP C'EST ECRIT NUL PART JVAIS PETER UNE DURITE
+		console.log(kekW)
 	});
 };
 
