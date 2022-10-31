9a80cb6b86138bfb4cce455927bda888a34b0d40
Denis Kazberuk <Deniskazberuk@hotmail.com>
2022-10-31 23:44:49 +0100

    RAGE QUIT AGANE

--- src/Pages/Lists.js
+++ src/Pages/Lists.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Listes = function () {
-	axios('http://localhost:2999/user/lists', {
-		method: 'get',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${localStorage.getItem('token')}`,
-		},
-	}).then((res) => {
-		console.log(res);
-	});
+	useEffect(()=>{
+		axios('http://localhost:2999/user/lists', {
+			method: 'get',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+		}).then((res) => {
+			console.log(res);
+		});
+	}, [])
+
 	return <div>Listes</div>;
 };
 
