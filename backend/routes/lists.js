6251abccaf5e806ccc8658b4846d7a09b8711cc1
Denis Kazberuk <Deniskazberuk@hotmail.com>
2022-10-31 23:24:36 +0100

    Forget to rechange de request.user

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -4,7 +4,7 @@ const lists = async function (fastify) {
 	fastify.register(auth);
 	const Lists = await fastify.mongo.db.collection('lists');
 	await fastify.get('/lists', async (request, reply) => {
-		const lists = await Lists.find({ owner: 1 }).toArray();
+		const lists = await Lists.find({ owner: request.user }).toArray();
 
 		const listFiltered = lists.map((element) => {
 			const { dict, Title } = element;
