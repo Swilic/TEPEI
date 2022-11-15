82870fecdada93d991a35a7b03eef90eca50f3ef
Denis Kazberuk <Deniskazberuk@hotmail.com>
2022-11-15 22:54:53 +0100

    Delete component not reused

--- src/Pages/Lists.js
+++ src/Pages/Lists.js
@@ -1,14 +1,64 @@
+import axios from 'axios';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useNavigate, Link } from 'react-router-dom';
 import Navigation from '../Components/Navigation.js';
-import RenderedLists from '../Components/RenderedLists.js';
+import ListsFetching from '../utils/ListsFetching.js';
 
 const Lists = () => {
+	const lists = ListsFetching();
+	const nav = useNavigate();
+
+	const toStudy = (element) => {
+		nav('/study', { state: element });
+	};
+
+	const deleteList = (title) => {
+		axios('http://localhost:2999/user/list', {
+			method: 'DELETE',
+			headers: {
+				authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+			data: { title },
+		}).then(() => {
+			if (res.data === 'Unvalid token!') {
+				Logout();
+				nav('/');
+			}
+			location.reload();
+		});
+	};
 	return (
 		<div>
 			<Navigation />
-			<Link to='/creation' className='create'>Nouvelle liste</Link>
-			<RenderedLists />
+			<Link to='/creation' className='create'>
+				Nouvelle liste
+			</Link>
+			<nav className='navLists'>
+				<ul className='lists'>
+					{lists.map((element) => {
+						return (
+							<div className='wrapLists' key={element.title}>
+								<li className='list'>
+									<NavLink to='list' state={element}>
+										<h2>{element.title}</h2>
+									</NavLink>
+									<button
+										onClick={() => {
+											deleteList(element.title);
+										}}>
+										Supprimer
+									</button>
+								</li>
+								<button
+									className='play'
+									onClick={() => toStudy(element)}>
+									Apprendre
+								</button>
+							</div>
+						);
+					})}
+				</ul>
+			</nav>
 		</div>
 	);
 };
