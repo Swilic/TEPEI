8f4502bc6c9a0c22cbb52766f67d8ff1f3830a4d
Denis <Deniskazberuk@hotmail.com>
2023-01-16 23:21:32 +0100

    fuuuuuuuuuuuuuuuuuck

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
 		event.preventDefault();
 		const user = userRef.current.value;
 		const pass = passRef.current.value;
-		axios('https://denisteamteam.netlify.app/account/login', {
+		axios('https://denisteam.netlify.app/account/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
