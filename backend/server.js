c984d5019215b79a6a9d9eea7871940f107739e9
Denis Kazberuk <Deniskazberuk@hotmail.com>
2023-01-20 19:54:51 +0100

    another one

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ fastify.register(cors, {
 });
 fastify.register(dbconnector);
 
+fastify.get('/', (request, reply) => {
+	reply.send({ hello: 'world' });
+});
 // Routes
 fastify.register(account, { prefix: '/account' });
 fastify.register(lists, { prefix: '/user' });
