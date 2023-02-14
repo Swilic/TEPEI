0d53a9f62b05dd4a24320e8dcffb98f2fcee05fa
Denis <Deniskazberuk@hotmail.com>
2023-02-14 18:19:04 +0100

    I think it's done!

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
 		const pass = passRef.current.value;
 
 		// Requête pour se connecter
-		axios('https://nodeserver-73b4.onrender.com/account/login', {
+		axios('https://somehting.onrender.com/account/login', {
 			method: 'POST',
 			headers: {
 				'Access-Control-Allow-Origin': '*',
