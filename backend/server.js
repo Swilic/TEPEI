55e042a6f4677ff44ebff27405c0c2cf0967fd00
Denis <Deniskazberuk@hotmail.com>
2023-04-30 01:08:02 +0200

    Modification ranndom port and host 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ fastify.register(account, { prefix: '/account' });
 fastify.register(lists, { prefix: '/user' });
 
 // Run the server!
-fastify.listen((err) => {
+fastify.listen({ port: 0, host: '0.0.0.0' }, (err) => {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
